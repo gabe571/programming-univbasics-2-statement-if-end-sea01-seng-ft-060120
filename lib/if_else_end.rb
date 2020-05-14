421188b9c70006bc30f5bd1fca0e69223afcd99c
Gabriel Castro <gabriel.castro1028@gmail.com>
2020-05-14 13:27:13 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,9 @@
-current_time = Time.now
+current_time = Time.now 
 current_time =
-current_time.to_i
+current_time.to_i 
 
-if current_time % 2 == 0 
+if current_time.even?
   puts "Even!"
-else puts "odd!"
+else
+  puts "Odd!"
 end
